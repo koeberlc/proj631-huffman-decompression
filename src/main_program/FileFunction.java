e23461398d738bae77bc50a092e1f06a03254d8c
esteban143 <celien.koeberle143@gmail.com>
2021-06-08 11:15:51 +0200

    All classes commented

--- src/main_program/FileFunction.java
+++ src/main_program/FileFunction.java
@@ -6,10 +6,23 @@ import java.util.*;
 import java.io.FileWriter;
 import java.io.IOException;
 
+/**
+ * <b>Class FileFuncion</b>
+ * <p>
+ * Global file management
+ * </p>
+ */
 public class FileFunction {
 	private String path;
 	private File file;
 
+	/**
+	 * @param path (<i>type: String</i>): the path to the file to which we want to perform various actions
+	 * 
+	 * <p>
+	 * Constructor
+	 * </p>
+	 */
 	public FileFunction(String path){
 		super();
 		this.path = path;
@@ -17,14 +30,29 @@ public class FileFunction {
 		
 	}
 
+	/**
+	 * @return path
+	 * 
+	 * <i>Path getter</i>
+	 */
 	public String getPath() {
 		return path;
 	}
+	/**
+	 * @param path
+	 * 
+	 * <p>Path setter</p>
+	 */
 	public void setPath(String path) {
 		this.path = path;
 	}
 	
 	
+	/**
+	 * @param text (<i>type: String</i>): the text content to write into the current file
+	 * 
+	 * <p>This method write a text passed in parameter into the current file</p>
+	 */
 	public void FileWriter(String text) {
 		try {
 			  file.createNewFile();
@@ -42,6 +70,11 @@ public class FileFunction {
 		    }
 	}
 	
+	/**
+	 * @return text (<i>type: String</i>)
+	 * 
+	 * <p>This method return the entire text into the current file</p>
+	 */
 	public String FileReader() {
 		String text = "";
 		try {
@@ -58,6 +91,12 @@ public class FileFunction {
 		return text;
 	}
 	
+	/**
+	 * @param minusLine (<i>type: boolean</i>): if we don't want to keep the first line
+	 * @return list (<i>type: List<\String></i>): a list of string containing a character with its frequency
+	 * 
+	 * <p>This method write for each line a string which will be added in a list.</p>
+	 */
 	public List<String> FileReader(boolean minusLine) {
 		List<String> list = new ArrayList<String>();
 		try {
